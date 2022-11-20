a58c3775193e9cde65edc450adc50e4b0b19a369
hassan <zare@yahoo.com>
2022-11-20 23:40:06 +0330

    End

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let pressure = document.querySelector(".weather__indicator--pressure>.value");
 let image = document.querySelector(".weather__image");
 let temperature = document.querySelector(".weather__temperature");
 let forecastBlock = document.querySelector(".weather__forecast");
+let suggestions = document.querySelector("#suggestions");
 
 let weatherAPIKey = "5271df1c028dc685653a2c7803491142";
 let weatherBaseEndpoint =
@@ -15,10 +16,59 @@ let weatherBaseEndpoint =
 let forecastBaseEndpoint =
   "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" +
   weatherAPIKey;
+let cityBaseEndpoint = "https://api.teleport.org/api/cities/?search=";
 
-let getWeatherByCityName = async (city) => {
+let weatherImages = [
+  {
+    url: "images/clear-sky.png",
+    ids: [800],
+  },
+  {
+    url: "images/few-cloud.png",
+    ids: [801],
+  },
+  {
+    url: "images/mist.png",
+    ids: [701, 711, 721, 731, 741, 751, 761, 771, 781],
+  },
+  {
+    url: "images/rain.png",
+    ids: [500, 501, 502, 503, 504],
+  },
+  {
+    url: "images/scattered-clouds.png",
+    ids: [802],
+  },
+  {
+    url: "images/shower-rain.png",
+    ids: [520, 521, 522, 531, 300, 301, 302, 310, 311, 312, 312, 314, 321],
+  },
+  {
+    url: "images/snow.png",
+    ids: [511, 600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622],
+  },
+  {
+    url: "images/thunderstorm.png",
+    ids: [200, 201, 202, 210, 211, 212, 221, 230, 231, 231],
+  },
+];
+
+let getWeatherByCityName = async (cityString) => {
+  let city;
+  if (cityString.includes(",")) {
+    city =
+      cityString.substring(0, cityString.indexOf(",")) +
+      cityString.substring(cityString.lastIndexOf(","));
+  } else {
+    city = cityString;
+  }
+
   let endpoint = weatherBaseEndpoint + "&q=" + city;
   let response = await fetch(endpoint);
+  if (response.status !== 200) {
+    alert("City not found!");
+    return;
+  }
   let weather = await response.json();
   return weather;
 };
@@ -41,18 +91,44 @@ let getForecastByCityID = async (id) => {
   return daily;
 };
 
+let weatherForCity = async (city) => {
+  let weather = await getWeatherByCityName(city);
+  if (!weather) {
+    return;
+  }
+  let cityID = weather.id;
+  updateCurrentWeather(weather);
+  let forecast = await getForecastByCityID(cityID);
+  updateForecast(forecast);
+};
+
+let init = () => {
+  weatherForCity("Tehran").then(() => (document.body.style.filter = "blur(0)"));
+};
+init();
+
 searchInp.addEventListener("keydown", async (e) => {
   if (e.keyCode === 13) {
-    let weather = await getWeatherByCityName(searchInp.value);
-    let cityID = weather.id;
-    updateCurrentWeather(weather);
-    let forecast = await getForecastByCityID(cityID);
-    updateForecast(forecast);
+    weatherForCity(searchInp.value);
   }
 });
 
+searchInp.addEventListener("input", async () => {
+  let endpoint = cityBaseEndpoint + searchInp.value;
+  let result = await (await fetch(endpoint)).json();
+  suggestions.innerHTML = "";
+  let cities = result._embedded["city:search-results"];
+  let length = cities.length > 5 ? 5 : cities.length;
+  for (let i = 0; i < length; i++) {
+    let option = document.createElement("option");
+    option.value = cities[i].matching_full_name;
+    suggestions.appendChild(option);
+  }
+
+  // console.log(result);
+});
+
 let updateCurrentWeather = (data) => {
-  console.log(data);
   city.textContent = data.name + "," + data.sys.country;
   day.textContent = dayOfWeek();
   humidity.textContent = data.main.humidity;
@@ -73,6 +149,13 @@ let updateCurrentWeather = (data) => {
     data.main.temp > 0
       ? "+" + Math.round(data.main.temp)
       : "-" + Math.round(data.main.temp);
+
+  let imgID = data.weather[0].id;
+  weatherImages.forEach((obj) => {
+    if (obj.ids.includes(imgID)) {
+      image.src = obj.url;
+    }
+  });
 };
 
 let updateForecast = (forecast) => {
